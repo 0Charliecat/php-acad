ba413c98c6689a2a75ba9ecb7233169df702d948
Charlie <meow@charliecat.space>
2023-10-28 09:06:37 +0200

    shows date and time + if is after 8:00am + dies if arrival after 20:00

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
-    $timestamp = time(); 
+    $timestamp = time();
     $currentDate = gmdate('d. m. Y H:i:s', $timestamp);
+
     echo "<h1>Ahoj • " . $currentDate . "</h1>";
+    echo "<code>".getLogs($timestamp)."</code>";
+
+    function getLogs($time) {
+        $Date = gmdate('d. m. Y', $time);
+        $Time = gmdate('H:i:s', $time);
+        $Meskanie = "";
+
+        if (intval(date("H")) >= 20) {
+            die("can't write an arrival after 20:OO");
+        }
+
+        if (intval(date("H")) >= 8) {
+            $Meskanie = " • mešká";
+        }
+        file_put_contents("prichody.txt", $Date." ".$Time.$Meskanie."\n", FILE_APPEND);
+        $fileconts = file_get_contents("prichody.txt");
+        return str_replace("\n", "<br>", $fileconts);
+    }
 ?>
